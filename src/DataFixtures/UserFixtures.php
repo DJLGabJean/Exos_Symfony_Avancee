cee5c20a9aaf5bbb0022ffde7e9f2e1ba93e88dd
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2025-01-18 09:48:53 +0100

    Mise à jour des fixtures de User

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,43 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Création d'un utilisateur administrateur
+        $admin = new User();
+        $admin->setEmail('admin@example.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword(
+            $this->passwordHasher->hashPassword($admin, 'admin123')
+        );
+        $manager->persist($admin);
+
+        // Création de plusieurs utilisateurs classiques
+        for ($i = 1; $i <= 10; $i++) {
+            $user = new User();
+            $user->setEmail("user$i@example.com");
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword(
+                $this->passwordHasher->hashPassword($user, 'password')
+            );
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
 }
+
