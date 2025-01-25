cefd5031d4235a2fa7d05d9df1634b86ed9e3a56
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2025-01-25 10:41:56 +0100

    Mise à jour du contrôleur de Task + Ajout d'une base pour son formulaire

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,17 +2,82 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class TaskController extends AbstractController
+#[Route('/tasks', name: 'task_')]
+class TaskController extends AbstractController
 {
-    #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(TaskRepository $taskRepository): Response
     {
+        $tasks = $taskRepository->findAll();
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks' => $tasks,
         ]);
+    }
+
+    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setCreatedAt(new \DateTimeImmutable());
+            $task->setUpdatedAt(new \DateTimeImmutable());
+            $task->setAuthor($this->getUser());
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('task_index');
+        }
+
+        return $this->render('task/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/{id}', name: 'view', methods: ['GET'])]
+    public function view(Task $task): Response
+    {
+        return $this->render('task/view.html.twig', [
+            'task' => $task,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Task $task, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setUpdatedAt(new \DateTimeImmutable());
+            $entityManager->flush();
+
+            return $this->redirectToRoute('task_index');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form->createView(),
+            'task' => $task,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'delete', methods: ['POST'])]
+    public function delete(Task $task, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('task_index');
     }
 }
